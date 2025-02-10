c95b5a9d7c86a0be6786e930f3d546d5fe1a9fee
@hexlet/cli <support@hexlet.io>
2025-02-10 13:01:09 +0500

    submit java-spring-boot-ru/jpa-auditing

---
+++ java-spring-boot-ru/jpa-auditing/src/main/java/exercise/model/Task.java
@@ -0,0 +1,40 @@
+package exercise.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+// BEGIN
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+
+@Entity
+@Table(name = "tasks")
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Task {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @CreatedDate
+    private LocalDate createdAt;
+    @LastModifiedDate
+    private LocalDate updatedAt;
+}
+// END
